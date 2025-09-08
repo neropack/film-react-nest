a24a5b5a77bb5447e3c5835586d5999ac0cd9a44
Dmitriy Meshcheryakov <WindlessMist@yandex.ru>
2025-09-08 23:36:10 +0300

    feat/dto

--- backend/src/films/dto/films.dto.ts
+++ backend/src/films/dto/films.dto.ts
@@ -1,1 +1,14 @@
 //TODO описать DTO для запросов к /films
+export class FilmDto {
+  id: string;
+  title: string;
+  description: string;
+  poster: string;
+}
+export class FilmScheduleDto extends FilmDto {
+  schedule: SessionDto[];
+}
+export class SessionDto {
+  id: string;
+  datetime: string;
+}
--- backend/src/order/dto/order.dto.ts
+++ backend/src/order/dto/order.dto.ts
@@ -1,1 +1,10 @@
 //TODO реализовать DTO для /orders
+export class SeatDto {
+  row: number;
+  seat: number;
+}
+export class CreateOrderDto {
+  filmId: string;
+  sessionId: string;
+  seats: SeatDto[];
+}
