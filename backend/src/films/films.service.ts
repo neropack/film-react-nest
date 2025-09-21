c0e6705c8a2a911fc008a2dcaa9816b5a70330cd
Dmitriy Meshcheryakov <WindlessMist@yandex.ru>
2025-09-22 00:13:26 +0300

    feat/ films error checks

--- backend/src/films/films.service.ts
+++ backend/src/films/films.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { FilmsRepository } from '../repository/films.repository';
 
 @Injectable()
@@ -6,19 +6,35 @@ export class FilmsService {
   constructor(private readonly filmRepository: FilmsRepository) {}
 
   async getAllFilms() {
-    const films = await this.filmRepository.findAll();
-    return {
-      total: films.length,
-      items: films,
-    };
+    try {
+      const films = await this.filmRepository.findAll();
+
+      return {
+        total: films.length,
+        items: films,
+      };
+    } catch (error) {
+      throw new InternalServerErrorException('Failed to fetch films');
+    }
   }
 
   async getFilmSchedule(id: string) {
-    const film = await this.filmRepository.findById(id);
-    if (!film) throw new NotFoundException('Film not found');
-    return {
-      total: film.schedule.length,
-      items: film.schedule,
-    };
+    try {
+      if(!id) throw new BadRequestException('Film ID is required');
+      
+      const film = await this.filmRepository.findById(id);
+      if (!film) throw new NotFoundException('Film not found');
+
+      return {
+        total: film.schedule.length,
+        items: film.schedule,
+      };
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+
+      throw new BadRequestException('Invalid film ID');
+    }
   }
 }
