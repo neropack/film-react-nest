f3bbc8f6f5aff7d85648032bb12d9c043ad12955
Dmitriy Meshcheryakov <WindlessMist@yandex.ru>
2025-09-21 15:47:22 +0300

    feat/import database

--- backend/src/app.config.provider.ts
+++ backend/src/app.config.provider.ts
@@ -8,6 +8,12 @@ export const configProvider = {
     database: {
       driver: process.env.DATABASE_DRIVER,
       url: process.env.DATABASE_URL,
+      type: process.env.DATABASE_TYPE as 'postgres' || 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
     },
   },
 };
@@ -19,4 +25,10 @@ export interface AppConfig {
 export interface AppConfigDatabase {
   driver: string;
   url: string;
+  type: string;
+  host: string;
+  port: string | number;
+  username: string;
+  password: string;
+  database: string;
 }
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,6 +6,9 @@ import * as path from 'node:path';
 import { configProvider } from './app.config.provider';
 import { FilmsModule } from './films/films.module';
 import { OrderModule } from './order/order.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Film } from './films/entitites/film.entity';
+import { Schedule } from './films/entitites/schedule.entity';
 
 @Module({
   imports: [
@@ -20,6 +23,16 @@ import { OrderModule } from './order/order.module';
     }),
     FilmsModule,
     OrderModule,
+    TypeOrmModule.forRoot({
+      type: process.env.DATABASE_TYPE as 'postgres' || 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [Film, Schedule],
+      synchronize: true,
+    })
   ],
   controllers: [],
   providers: [configProvider],
