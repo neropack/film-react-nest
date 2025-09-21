c2583dd4e025ef6ade567b40ed36eead8caf15f2
Dmitriy Meshcheryakov <WindlessMist@yandex.ru>
2025-09-21 19:23:19 +0300

    feat/import db in modules

--- backend/src/films/films.module.ts
+++ backend/src/films/films.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { FilmsController } from './films.controller';
 import { FilmsService } from './films.service';
 import { FilmsRepository } from 'src/repository/films.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Film } from './entitites/film.entity';
+import { Schedule } from './entitites/schedule.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Film, Schedule])],
   controllers: [FilmsController],
   providers: [FilmsService, FilmsRepository],
   exports: [FilmsService],
--- backend/src/order/order.module.ts
+++ backend/src/order/order.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
 import { FilmsRepository } from 'src/repository/films.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Film } from 'src/films/entitites/film.entity';
+import { Schedule } from 'src/films/entitites/schedule.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Film, Schedule])],
   controllers: [OrderController],
   providers: [OrderService, FilmsRepository],
   exports: [OrderService],
