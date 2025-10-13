64b96106a6895c70ff5b203cecc57bce73c57683
Dmitriy Meshcheryakov <WindlessMist@yandex.ru>
2025-10-13 17:18:55 +0300

    feat/ dev logger

---
+++ backend/src/logger/dev.logger.ts
@@ -0,0 +1,8 @@
+import { ConsoleLogger, Injectable } from "@nestjs/common";
+
+@Injectable()
+export class DevLogger extends ConsoleLogger {
+    log(message: any, ...optionalParams: any[]) {
+        super.log(message, ...optionalParams);
+    }
+}
